a6675137bd9955274dbb40ca18188aaf45dfeb03
Bolton-A <112293016+Bolton-A@users.noreply.github.com>
2022-10-22 15:32:03 -0400

    Added my project

--- flask/base.py
+++ flask/base.py
@@ -1,90 +1,54 @@
 '''
-Last updated: 10/10/2022
-Description: Allows Python calculations to be performed and made available to website.
-TODO:
+Last updated: 10/21/2022
+Description: Code connects to PostgreSQL through psycopg2, uses 'myVar' variable sent by App.js to execute a query to the database.
+Once query has been executed, returns the results to 'results.csv' and closes the database.
+
 '''
 
 # Import necessary libraries.
-from flask import Flask
+from flask import Flask, request
 import psycopg2
+import csv
 
 # Creates an instance of the web application under the name 'api'.
 api = Flask(__name__)
 
-#establishing the connection
-conn = psycopg2.connect(
-   database="postgres", user='postgres', password='postgrespw', host='host.docker.internal', port= '49153'
-)
-#Setting auto commit false
-conn.autocommit = True
-
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
-
-sql = '''SELECT concat(FirstName, ' ', LastName) FROM Logins WHERE UserID = 'ls123';'''
-
-#Retrieving data
-cursor.execute(sql)
-
-#Fetching all from the table
-name = cursor.fetchall();
-
-# Sets a dictionary of items that will appear within the /profile link. Returns dictionary to site.
-##### To add more items, add below and modify ./App.js to include items.
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name" : name,
-        "about" : "Hello! This is sample text!"
-    }
-
-    return response_body
-
-#Commit your changes in the database
-conn.commit()
-
-#Closing the connection
-conn.close()
-
 '''
-# Import necessary libraries.
-from flask import Flask
-import psycopg2
-
-# Creates an instance of the web application under the name 'api'.
-api = Flask(__name__)
-
-#establishing the connection
-conn = psycopg2.connect(
-   database="postgres", user='postgres', password='postgrespw', host='host.docker.internal', port= '49153'
-)
-#Setting auto commit false
-conn.autocommit = True
-
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
-
-#Retrieving data
-cursor.execute(''SELECT * FROM Logins'') # Add '  ' around SELECT * FROM Logins
-
-#Fetching all from the table
-result = cursor.fetchall();
-
-# Sets a dictionary of items that will appear within the /profile link. Returns dictionary to site.
-##### To add more items, add below and modify ./App.js to include items.
-@api.route('/profile')
-def my_profile():
-    response_body = {
-        "name" : "Laura Smith",
-        "about" : "Hello! This is sample text!",
-        "login" : result
-    }
-
-    return response_body
-
-#Commit your changes in the database
-conn.commit()
-
-#Closing the connection
-conn.close()
+Section: Query Database
+Description: Creates query and sends results to 'results.csv'.
 '''
+@api.route('/profile', methods=['POST', 'GET'])
+def myFunction():
+
+    # Creates a connection to the database, establishes a cursor, takes 'myVar' variable from JavaScript and executes query in PostgreSQL.
+    conn = psycopg2.connect(database="postgres", user='postgres', password='postgres', host='localhost', port= '5432')
+    conn.autocommit = True
+    cursor = conn.cursor()
+    query = request.form['myVar']
+    cursor.execute(query)
+    rows = cursor.fetchall();
+
+    # If query returns anything, create list of result and column_names. Append each to two seperate lists.
+    if rows:
+        result = list()
+        column_names = list()
+        for i in cursor.description:
+            column_names.append(i[0])
+        result.append(column_names)
+        for row in rows:
+            result.append(row)
+
+    # Ensuring that results.csv is own, write the rows recorded in the lists in the section above.
+    with open('results.csv', 'w', encoding='UTF8', newline='') as f:
+        writer = csv.writer(f)
+        for row in result:
+            writer.writerow(row)
+
+    # Commit changes and close database.
+    conn.commit()
+    conn.close()
+    return result
+
+# Ensure that app is running.
+if __name__ == '__main__':
+    api.run(host="localhost", port=3000, debug=True)
--- src/App.js
+++ src/App.js
@@ -1,53 +1,324 @@
 /*
-Last updated: 10/10/2022
+Last updated: 10/21/2022
 Description: Sets up content that is taken from ./base.py and is used in website.
-TODO:
 */
 
-/* Import necessary libraries. */
-import { useState } from 'react'
-import axios from "axios";
-import './App.css';
+// Import necessary libraries.
+import { Input, Collapse, Typography, Empty, Divider } from 'antd';
+import React from 'react';
+import Papa from "papaparse";
+import "./App.css";
 
-/* Run an app that builds values that appear on website. */
-function App() {
-  const [profileData, setProfileData] = useState(null)
-  function getData() {
-    axios({
-      method: "GET",
-      url:"/profile",
-    })
-    .then((response) => {
-      const res = response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        }
-    })}
+// Declare constants.
+const { Panel } = Collapse;
+const { Text } = Typography;
+const { TextArea } = Input;
 
-  /* Set up pre-existing outputs that appear on the website and are not modified
-  by the Python code. */
+const App = () => {
+
+  // Calls the function to prepare to create a table when csv is loaded. (Described further below.)
+  populateToTable()
+
+  /*
+  Section: Clear Web Application
+  Description: Refreshes window, clearing the table and the query input section.(DOES NOT CLEAR DATABASE/results.csv).
+  Provides the user with a prompt informing user of what will happen and asking if they would like to contionue before refreshing.
+  */
+  function clearWebApp() 
+  {
+    // If the user confirms then the window will refresh. Displays success message.
+    if (window.confirm('Are you sure you want to clear the table and your query?')) {
+      window.location.reload()
+      console.log('Table and query have been cleared.');
+    } 
+    // If the user refuses then the window will remain the same.
+    else 
+    {
+      console.log('Table and query have been spared.');
+    }
+  }
+
+  /*
+  Section: Clear Database
+  Description: Performs the above function in refreshing the page to clear the table and the query input section. Then sends a query to remove all
+  records within the Login table. Like Clear Web Application, it will explain what will happen and asks if the user would like to continue or not.
+  */
+  function clearDatabase()
+  {
+    // If the user confirms then the window will refresh.
+    if (window.confirm('Are you sure you want to remove everything from the database and your current query?')) 
+    {
+      window.location.reload()
+      console.log('Database has been emptied.');
+    } 
+    // If the user refuses then the window will remain the same.
+    else 
+    {
+      console.log('Database has been spared.');
+    }
+    // Sets 'myVar' to a query that removes all entries and sends to Flask to execute. Displays success message.
+    myVar = 'DELETE FROM Logins;'
+        const request = new XMLHttpRequest();
+        request.open('POST', '/profile', false);
+        request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        request.onreadystatechange = function() {};
+        request.send('myVar=' + myVar);
+        alert("Your database was successfully cleared!");
+  }
+
+  /*
+  Section: Populate To Table
+  Description: Code waits for csv file to be uploaded from index.html side. Then checks to ensure that the uploaded file is a csv before parsing file
+  where it detects a set of characters to seperate entries. Then builds the table by its headers and subsequent rows.
+  */
+  function populateToTable()
+  {
+
+    // Takes input from index.html and validates it.
+    const readSingleFile = () => 
+    {
+      const fileInput = document.getElementById("fileinput");
+      const form = document.getElementById("controls-form");
+    
+      // Validates file as being of the csv type. Alerts user if they select a file that is incorrect.
+      if (fileInput.files[0].type !== "text/csv") 
+      {
+        alert("Not a CSV file");
+        form.reset();
+      }
+      // If correct, code proceeds to next section.
+      else 
+      {
+        parseCSV(fileInput.files[0]);
+      }
+    };
+    
+    // Parses csv file for characters to seperate entries. Sets to rows and headers if valid and begins parse with PapaParse.
+    const parseCSV = file => 
+    {
+      const config = 
+      {
+        delimiter: "",
+        newline: "",
+        quoteChar: '"',
+        dynamicTyping: false,
+        preview: 0,
+        encoding: "",
+        worker: false,
+        comments: false,
+        step: undefined,
+        complete: function(response) 
+        {
+          console.log('loaded')
+          const headersArray = response.meta.fields;
+          const rows = response.data;
+          buildTable(headersArray, rows);
+        },
+        error: (error) => alert(error),
+        download: false,
+        skipEmptyLines: false,
+        chunk: undefined,
+        fastMode: undefined,
+        beforeFirstChunk: () => 
+        {
+          console.log('loading...')
+        },
+        withCredentials: undefined
+      };
+      Papa.parse(file, config);
+    };
+    
+    // Builds table using the headers and rows retrieved above.
+    const buildTable = (headersArray, rows) => 
+    {
+      console.log('rendering started')
+
+      // Sets table headers.
+      let tableHeader = document.getElementById("csv-table__header");
+      tableHeader.innerHTML = "";
+    
+      // Sets table body.
+      let tableBody = document.getElementById("csv-table__body");
+      tableBody.innerHTML = "";
+      
+      // Builds table headers. If headersArray has some value, go through each value and append.
+      let headerFragment = document.createDocumentFragment();
+      if (headersArray)
+      {
+        headersArray.forEach((header, index) => 
+        {
+          let th = document.createElement("th");
+          let txt = document.createTextNode(headersArray[index]);
+          th.appendChild(txt);
+          headerFragment.appendChild(th);
+        });
+        tableHeader.appendChild(headerFragment);
+      }
+    
+      // Builds table body. If row has some value, go through each value and append.
+      let rowsFragment = document.createDocumentFragment();
+      console.log('Rows:', rows.length);
+      rows.forEach((row, index) => 
+      {
+        var tr = document.createElement("tr");
+        Object.values(row).forEach(item => {
+          let td = document.createElement("td");
+          let txt = document.createTextNode(item);
+          td.appendChild(txt);
+          tr.appendChild(td);
+          rowsFragment.appendChild(tr);
+        });
+        tableBody.appendChild(rowsFragment);
+      });
+
+      // Display success message.
+      console.log('rendering finished');
+    };
+    
+    // Listens to events from index.html.
+    document
+      .getElementById("fileinput")
+      .addEventListener("change", readSingleFile, false);
+  }  
+
+  // Sets up myVar.
+  let [myVar, setMyVar] = React.useState('');
+
+  /*
+  Section: Send Request
+  Description: Creates XMLHttpRequest to send myVar to base.py.
+  */
+  function sendRequest()
+  {
+    const request = new XMLHttpRequest();
+    request.open('POST', '/profile', false);
+    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    request.onreadystatechange = function(){};
+    request.send('myVar=' + myVar);
+  }
+
+  /*
+  Section: Post Data
+  Description: Calls for Send Request to send myVar to base.py and displays success message.
+  */
+  function postData()
+  {
+    sendRequest()
+    alert("Your SQL was successfully submitted!");
+  }
+
+  /*
+  Section: View All
+  Description: Calls for Send Request to send myVar as 'SELECT * FROM Logins;' to base.py and displays success message.
+  This causes all entries within the Login table to be sent to the csv.
+  */
+  function viewAll()
+  {
+    myVar = 'SELECT * FROM Logins;'
+    sendRequest()
+    alert("Your query was successfully submitted!");
+  }
+
+  /*
+  Section: Five Sample
+  Description: Calls for Send Request to send myVar as an INSERT INTO that generates five sample entries to base.py and displays success message.
+  */
+  function fiveSample()
+  {
+    myVar = "INSERT INTO Logins(username, password, useremail, tickettime) VALUES ('LauraSmith', 'laura123', 'laura@gmail.com', 2), ('BobRoss', 'bs456', 'bob@gmail.com', 2), ('HannahSmith', 'hs789', 'hannah@gmail.com', 2), ('SamSamson', 'ss012', 'sam@gmail.com', 2), ('RonRob', 'ron345', 'ron@gmail.com', 2);"
+    sendRequest()
+    alert("You are ready to query this sample!");
+  }
+
+  /*
+  Section: Twenty Sample
+  Description: Calls for Send Request to send myVar as an INSERT INTO that generates twenty sample entries to base.py and displays success message.
+  */
+  function twentySample()
+  {
+    myVar = "INSERT INTO Logins(username, password, useremail, tickettime) VALUES ('LauraSmith', 'ls123', 'laura@gmail.com', 2), ('BobRoss', 'bs456', 'bob@gmail.com', 2), ('HannahSmith', 'hs789', 'hannah@gmail.com', 2), ('SamSamson', 'ss012', 'sam@gmail.com', 2), ('RonRob', 'rr345', 'ron@gmail.com', 2), ('TinaNash', 'tn678', 'tina@gmail.com', 2), ('LutherTodd', 'lt901', 'luther@gmail.com', 2), ('GertrudeRamirez', 'gr234', 'gertrude@gmail.com', 2), ('ErvinPadilla', 'ep567', 'ervin@gmail.com', 2), ('TerrenceClark', 'tc890', 'terraence@gmail.com', 2), ('DaveFreeman', 'df123', 'dave@gmail.com', 2), ('JoseWalt', 'jw456', 'jose@gmail.com', 2), ('KariGutierrez', 'kg789', 'kari@gmail.com', 2), ('LeonardFrank', 'lf012', 'leonard@gmail.com', 2), ('SilviaRoss', 'sr345', 'silvia@gmail.com', 2), ('DarinFletcher', 'df678', 'darin@gmail.com', 2), ('HoraceCarrol', 'hc901', 'horace@gmail.com', 2), ('EarlSwanson', 'es234', 'earl@gmail.com', 2), ('VanessaWoods', 'vw567', 'vanessa@gmail.com', 2), ('RubyEdwards', 're890', 'ruby@gmail.com', 2);"
+    sendRequest()
+    alert("You are ready to query this sample!");
+  }
+
+  // Checks if a change has occurred. (Used for collapsible elements.)
+  const onChange = (key) => 
+  {
+    console.log(key);
+  };
+
   return (
-    <div className = "App">
-      <header className = "App-header">
-          <h1>Hello!</h1>
-          <p>Welcome to my data-scrubbing application.</p>
+    <>
+      {/* Introduction section, displays title of application and creator. */}
+      <Divider>Open Avenues Datascrubbing Application</Divider>
+      <Text><p>This application was created by Aine Bolton during her Open Avenues Datascrubbing Application micro-internship. For more information, please see the Documentation tab.</p></Text>
+      
+      {/* Collapsable section. Displays usage and documentation. */}
+      <Collapse defaultActiveKey={['0']} onChange={onChange}>
+        <Panel header="Documentation" key="1">
+          <Text><p>The purpose of this application is to allow a user to create a query into a table named <Text mark>Logins</Text> and then return the result of that query.</p></Text>
+          <Text><p>To use this application:</p></Text>
+          <Text><p>1. Enter your SQL query in the section above  <Text code>Submit SQL</Text>.</p></Text>
+          <Text><p>2. Press <Text code>Submit SQL</Text>.</p></Text>
+          <Text><p>3. Click <Text code>Choose File</Text> and browse until you locate <Text code>results.csv</Text>. (Tip: You can find it in the <Text mark>.../datascrubbing/flask</Text> folder.)</p></Text>
+          <Text><p>4. View the result from your query below.</p></Text>
+          <Text><p>5. Press <Text code>Reset Table & Query Section</Text> and click <Text code>Yes</Text> in the prompt to type and submit a new query.</p></Text>
+          <Text><p>Tip: Use the <Text code>Pregenerated Commands</Text> tab for a few basic sample queries and sample tables.</p></Text>
+          
+          {/* Collapsable section. Displays troubleshooting guide. */}
+          <Collapse defaultActiveKey={['0']} onChange={onChange}>
+            <Panel header="Troubleshooting" key="1">
+              <Text><p>This guide is intended to provide assistance to users in the event that an error occurs with this application.</p></Text>
+              <Text><p><Text strong>Problem: "I clicked <Text code>Submit SQL</Text> but I can't see my table."</Text></p></Text>
+              <Text><p>Solution: After submitting your query through <Text code>Submit SQL</Text>, you must click <Text code>Choose File</Text> and select <Text code>results.csv</Text>. This will display the table.</p></Text>
+              <Text><p><Text strong>Problem: "I tried to select all results in the table but the table came back empty.</Text></p></Text>
+              <Text><p>Solution: Your table is probably empty. You can either select <Text code>Pregenerated Commands</Text> and one of the <Text code>Insert Sample Entries</Text> commands or create an INSERT INTO using the SQL query function to make your own table.</p></Text>
+              <Text><p><Text strong>Problem: "Why can't I upload the updated table?"</Text></p></Text>
+              <Text><p>Solution: To refresh the table, please click <Text code>Reset Table & Query Section</Text>. Either copy and paste your query to the refreshed page or send the query prior to refreshing. If you send it before then it should still work just fine!</p></Text>
+            </Panel>
+          </Collapse>
+        </Panel>
+      </Collapse>
 
-        <p>Try clicking the button below: </p>
-        <button onClick = {getData}>Click me!</button>
-        {profileData && <div>
-              <p>Profile name: {profileData.profile_name}</p>
-              <p>About me: {profileData.about_me}</p>
-            </div>
-        }
-      </header>
-    </div>
+      {/* Collapsable section. Displays pregenerated command buttons. */}
+      <Collapse defaultActiveKey={['0']} onChange={onChange}>
+        <Panel header="Pregenerated Commands" key="1">
+          {/* Loads results.csv with all entries from Logins. */}
+          <button type="button" class="submit" onClick = {viewAll}>Select All Logins</button>
+          {/* Loads five sample entries into Logins. */}
+          <button type="button" class="submit" onClick = {fiveSample}>Insert Five Sample Entries</button>
+          {/* Loads twenty sample entries into Logins. */}
+          <button type="button" class="submit" onClick = {twentySample}>Insert Twenty Sample Entries</button>
+          {/* Deletes everything in Logins. */}
+          <input type="button" class="submit" id="btnDelete" value="Clear Database" onClick={clearDatabase}/>
+        </Panel>
+      </Collapse>
+    
+      {/* Creates querying section. Automatically updates myVar on change. */}
+      <TextArea rows={4} 
+        placeholder="Hello! Here is where you will enter your SQL queries." 
+        maxLength={2000} 
+        onChange={(e) => setMyVar(e.target.value)}
+      />
+
+      {/* Sends user's SQL query to base.py for evaluation. */}
+      <button type="button" class="submit" onClick = {postData}>Submit SQL</button>
+      
+      {/* Refreshes page, clearing the table and SQL query. */}
+      <input type="button" class="submit" id="btnDelete" value="Reset Table & Query Section" onClick={clearWebApp}/>
+
+      {/* Creates table with empty section, instructing the user how to proceed. */}
+      <table id="csv-table">
+        <thead>
+          <tr id="csv-table__header">
+            <Empty class='empty'>
+              <Text><p>Enter your query, press <Text code>Submit SQL</Text> then click <Text code>Choose File</Text> to get started!</p></Text>
+            </Empty> 
+          </tr>
+        </thead>
+        <tbody id="csv-table__body"></tbody>
+      </table>
+    </>
   );
-}
-
+};
 export default App;
